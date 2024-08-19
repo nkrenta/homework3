8bfe0516343a79e4256490e9a90c9079353caf78
nkrenta <nkrenta@inbox.ru>
2024-08-19 23:15:12 +0300

    init

---
+++ src/hw3.java
@@ -0,0 +1,7 @@
+public class hw3 {
+    public static void main(String[] args) {
+
+        System.out.println("Hellow, world)");
+
+    }
+}
---
+++ src/lesson.java
@@ -0,0 +1,18 @@
+public class lesson {
+    public static void main(String[] args) {
+
+       byte bananas = 100;
+        System.out.println("Бананов " + bananas + " кг!");
+
+       float sugar = 3;
+       float onePortion = sugar / 4;
+        System.out.println("Одна порция равна " + onePortion);
+
+        byte a = 1;
+        short b =1;
+        int c = 1;
+        int d = a + b + c;
+
+        float g = a + 1f;
+    }
+}
